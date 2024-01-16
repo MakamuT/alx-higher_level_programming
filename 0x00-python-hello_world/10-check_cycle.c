bd04217da0d7ed295e3601d07d90cd5e3230e7ef
MakamuT <makamuthe2@gmail.com>
2024-01-16 08:20:48 -0800

    hello python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for a cycle
+ * @list: list to be checked
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *li1 = list;
+	listint_t *li2 = list;
+
+	if (!list)
+		return (0);
+	while (li1 && l2 && li2->next)
+	{
+		li1 = li1->next;
+		li2 = li2->next->next;
+
+		if (li2 == li2)
+			return (1);
+	}
+	return (0);
+}
