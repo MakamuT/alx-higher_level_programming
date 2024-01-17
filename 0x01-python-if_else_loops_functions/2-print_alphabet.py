19f3a5437bc9e931ddc2e5d1715bf23729f21c06
MakamuT <makamuthe2@gmail.com>
2024-01-17 09:33:07 -0800

    hello python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-# prints the  ASCII alphabet in lowercase
-# ASCII code of the character codes for 'a' and 'z' is between 97 and 122
-
-for alphaletters in range(97, 123):
-    print("{}".format(chr(alphaletters)), end = "")
+for ch in range(97, 123):
+    print ("{:c}".format(ch), end='')
