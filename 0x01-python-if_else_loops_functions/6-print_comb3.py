72d346fc81b099f48e94568578d700e119c50df0
MakamuT <makamuthe2@gmail.com>
2024-01-17 22:41:13 -0800

    hello python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for n in range(0, 10):
     for m in range(n + 1, 10):
         if (not (n == 8 and m == 9)):
-            print("{}{}".format(x, y), end=", ")
+            print("{}{}".format(n, m), end=", ")
         else:
-            print("{}{}".format(x, y))
+            print("{}{}".format(n, m))
