4abdc35892fa26f6923aee7dfc7dfc1f8480af95
MakamuT <makamuthe2@gmail.com>
2024-01-18 12:57:36 -0800

    hello python

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: integer1
+        b: integer2
+        Returns:
+        the value of a + b
+    """
+    return(a+b)
