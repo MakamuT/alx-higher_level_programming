6b533b5e2632da1bdac4ecdb85fb7eecda3a8fc1
MakamuT <makamuthe2@gmail.com>
2024-02-21 14:00:09 -0800

    file i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of characters writtten
+    """writes a string to a text file (UTF8) and returns the number
+    of characters writtten
     Args:
         filename - name of the file to write to.
         text(str) - the string to write
