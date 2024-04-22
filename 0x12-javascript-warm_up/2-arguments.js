de10d2578b1e1119cc075afdc5cbd54f2d22e00d
MakamuT <makamuthe2@gmail.com>
2024-04-22 11:26:17 -0700

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,6 @@
 // prints a message depending of the number of arguments passed
 
 const args = process.argv.slice(2);
-const message = args.lenght === 0 ? 'No argument' : (args.lenght === 1 ? 'Argument found' : 'Arguments found');
+const message = args.length === 0 ? 'No argument' : (args.length === 1 ? 'Argument found' : 'Arguments found');
 
 console.log(message);
