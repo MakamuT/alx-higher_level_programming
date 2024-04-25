70b59201d9f69ae18e63dff75caadb63a5e3e32c
MakamuT <makamuthe2@gmail.com>
2024-04-25 13:30:50 -0700

    JavaScript

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let num = 0;
-  for (let a = 0; a < list.lenght; a++) {
+  for (let a = 0; a < list.length; a++) {
     if (list[a] === searchElement) {
       num += 1;
     }
