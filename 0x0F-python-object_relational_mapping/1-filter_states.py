211c12f7c0f58cfb2d1790ea9728dfc44ee2e5df
MakamuT <makamuthe2@gmail.com>
2024-04-26 14:57:56 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,13 @@ import sys
 import MySQLdb
 
 if __name == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(stae) for state in c.fetchall() if state[1][0] == "N"]
+    c.execute("SELECT * FROM `states`  WHERE `name` LIKE BINARY 'N%' ORDER BY states.id ASC")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+        c.close()
+        db.close()
