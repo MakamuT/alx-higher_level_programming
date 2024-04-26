00ded470096e4adb139f6c701ce3b3b4550a2401
MakamuT <makamuthe2@gmail.com>
2024-04-26 15:25:55 -0700

    SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all
+   values in the states table of hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+    c = db.cursor()
+    c.exexcute("""SELECT * FROM states WHERE name LIKE BINARY
+               '{}' ORDER BY states.id ASC""".format(sys.argv[4]))
+    rows = c.fecthall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
