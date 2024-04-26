2c2356839844e9c094ce4007274b48cc2a7b7b51
MakamuT <makamuthe2@gmail.com>
2024-04-26 15:10:28 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" ists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+""" ists all states with a name starting with N """
 
 import sys
 import MySQLdb
 
 if __name == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     c = db.cursor()
-    c.execute("SELECT * FROM `states`  WHERE `name` LIKE BINARY 'N%' ORDER BY states.id ASC")
+    c.execute("""SELECT * FROM states  WHERE name
+              LIKE BINARY 'N%' ORDER BY states.id ASC""")
     rows = c.fetchall()
     for row in rows:
         print(row)
