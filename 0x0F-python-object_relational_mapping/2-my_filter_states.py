23bffaf1f8670db96672107d25da1a53a83461b1
MakamuT <makamuthe2@gmail.com>
2024-04-27 08:36:45 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ hbtn_0e_0_usa where name matches the argument
 import MSQLdb as db
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """access to db and get states"""
     db_connect = db.connect(host="localhost", port=3306, user=argv[1],
                             passwd=argv[2], db=argv[3])
