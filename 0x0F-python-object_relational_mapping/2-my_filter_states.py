50785738dfbd40b155a66d4a69a891d8ccf1b005
MakamuT <makamuthe2@gmail.com>
2024-04-27 08:45:38 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,7 @@ takes in an argument and displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument
 """
 
-import MSQLdb as db
+import MySQLdb as db
 from sys import argv
 
 if __name__ == '__main__':
@@ -12,8 +12,7 @@ if __name__ == '__main__':
     db_connect = db.connect(host="localhost", port=3306, user=argv[1],
                             passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'
-                      ORDER BY states.id ASC".format(argv[4]))
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4]))
     rows_selected = db_cursor.fetchall()
     for row in rows_selected:
         print(row)
