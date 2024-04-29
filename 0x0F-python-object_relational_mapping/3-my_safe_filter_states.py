6708b650db425a7b9fe374b8834cf8dbf3e5d90d
MakamuT <makamuthe2@gmail.com>
2024-04-29 10:28:45 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-write a script that takes in arguments and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument.
+write a script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!
 """
 
@@ -14,7 +14,8 @@ if __name__ == '__main__':
                             passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
     db_cursor.execute("""SELECT * FROM states WHERE name
-                      LIKE BINARY %(name)s ORDER BY states.id ASC""", {'name' : argv[4]})
+                      LIKE BINARY %(name)s ORDER BY states.id ASC""",
+                      {'name': argv[4]})
     rows_selected = db_cursor.fetchall()
     for row in rows_selected:
-       print(row)
+        print(row)
