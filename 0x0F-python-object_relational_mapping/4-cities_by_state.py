5ad6f18b6ad13286f2676c2492b91eff8eec6a85
MakamuT <makamuthe2@gmail.com>
2024-04-29 11:32:29 -0700

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,13 +5,12 @@ import MySQLdb as db
 from sys import argv
 
 if __name__ == '__main__':
-    
     """access the database"""
     db_connect = db.connect(host='localhost', port=3306, user=argv[1],
                             passwd=argv[2], db=argv[3])
     with db_connect.cursor() as db_cursor:
-        db_cursor.execute("""SELECT cities.id, cities.name, states.name \
-                          FROM cities.state_id \ = states.id
+        db_cursor.execute("""SELECT cities.id, cities.name, states.name
+                          FROM cities.state_id = states.id
                           ORDER BY cities.id ASC""")
     rows_selected = db_cursor.fetchall()
     if rows_selected:
