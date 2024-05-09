97947125f504de499fa8f9e3a2ac2edf39447620
MakamuT <makamuthe2@gmail.com>
2024-05-09 16:08:59 -0700

    HTTP/URL/HTTP Cookie

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    if list_of_int:
+        list_of_int.sort(reverse=True)
+        return list_of_int[0]
