6a2bec1e16daef502db2e1b7b785d82f5eb5ae11
MakamuT <makamuthe2@gmail.com>
2024-05-12 08:49:52 -0700

    URLLIB

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,18 +3,24 @@
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body
 """
-
+from sys import argv
+import urllib.request
+import urllib.parse
 
 if __name__ == '__main__':
-    import sys
-    import urllib.request
-    import urllib.parse
+    if len(argv) != 3:
+        print("Usage: ./script.py <url> <email>")
+        sys.exit(1)
 
-    DATA = urllib.parse.urlencode({"email": email})
-    DATA = DATA.encode('ascii')
     url = argv[1]
-    argv = sys.argv
     email = argv[2]
 
-    with urllib.request.urlopen(url, DATA) as response:
-        print(response.read().decode('utf-8'))
+    DATA = urllib.parse.urlencode({"email": email}).encode('ascii')
+
+    try:
+        with urllib.request.urlopen(url, DATA) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.URLError as e:
+        print(f"Error: {e.reason}")
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Errir: {e.code} {e.reason}")
