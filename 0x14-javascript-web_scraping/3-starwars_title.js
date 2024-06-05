ae24781d7e4749f9f60bb94db9e1978f741f7f24
MakamuT <makamuthe2@gmail.com>
2024-06-05 20:10:17 +0000

    using APIs

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request.get(url, (error, response, body) => {
   if (error) {
-   console.log(error);
+    console.log(error);
   } else {
     const content = JSON.parse(body);
     console.log(content.title);
